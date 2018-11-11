5bc4bc7a44dc6c22837c5ea948bbb0e2fff956ae
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-10 21:30:39 -0600

    orm first draft

--- config/orm.js
+++ config/orm.js
@@ -8,5 +8,24 @@ const orm = {
             console.log(result);
         });
     },
-    insertOne: function()
+    insertOne: function(column, value) {
+        let queryString = "INSERT INTO vue_todo_db (??) VALUES (?)";
+        connection.query(queryString, column, value, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function(bool, itemName) {
+        let queryString = "UPDATE vue_todo_db SET completed = ? WHERE list_item = ?";
+        connection.query(queryString, bool, itemName, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    end: function() {
+        connection.end();
+    }
 }
+
+
+module.exports = orm;
