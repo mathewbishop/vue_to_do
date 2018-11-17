e4eb87dde6c0f67d8ce94a8b2bf1dd2b67225a3b
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-16 22:17:04 -0600

    refactor: routes to be more RESTful, DRYing out the front end

--- controller/list_controller.js
+++ controller/list_controller.js
@@ -5,7 +5,10 @@ const express = require("express");
 const list = require("../model/list");
 const router = express.Router();
 
-router.post("/api/add-item", (req, res) => {
+//=============================================================
+// Routes
+//=============================================================
+router.post("/api/todolist", (req, res) => {
 // console.log(req.body.list_item);
     let newItem = req.body.list_item
     if (newItem === "") {
@@ -17,19 +20,19 @@ router.post("/api/add-item", (req, res) => {
             res.json(newItem)
         })
     }
-    
-        
+      
 });
 
-router.put("/api/list/item-update", (req, res) => {
+router.put("/api/todolist/:id", (req, res) => {
     list.updateOne(1, req.body.list_item, function(data) {
         console.log(data);
+        res.json(data);
     })
-    res.sendStatus(201);
+    
 })
 
-router.delete("/api/list/:item", (req, res) => {
-    let item = req.params.item;
+router.delete("/api/todolist/:id", (req, res) => {
+    let item = req.params.id;
     list.deleteOne(item, function(data) {
         console.log(data);
     })
@@ -37,17 +40,12 @@ router.delete("/api/list/:item", (req, res) => {
     res.end()
 })
 
-router.get("/api/list/not-complete", (req, res) => {
-    list.selectAll(0, function(data) {
+router.get("/api/todolist", (req, res) => {
+    list.selectAll(function(data) {
         res.json(data);
     })
 });
 
-router.get("/api/list/complete", (req, res) => {
-    list.selectAll(1, function(data) {
-        res.json(data);
-    })
-});
 
 
 module.exports = router;
